72ff7a1e6d760b127364d80ad22b9af4a93dd014
chrisk <chris.kim@unicity.com>
2025-04-04 13:55:22 -0600

    Fix: Type added.

--- src/components/tables/SeasonsTable.tsx
+++ src/components/tables/SeasonsTable.tsx
@@ -197,7 +197,7 @@ export const SeasonsTable = ({ seasonsData, hiddenFields, hideColumn }: SeasonsT
             <TableCell colSpan={seasonColumns.length}>Currently No Data to show</TableCell>
           </TableRow>
         )}
-        <VariableSizeList
+        <VariableSizeList<any>
           className="overscroll-auto mb-[50px] scrollbar-none"
           height={height}
           itemCount={seasonsData.length}
