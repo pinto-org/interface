8c2c4610ac6d5071358e3b37d8bbd79a3e529ad3
burr <burr.nim@proton.me>
2025-09-27 15:56:20 +0900

    feat: update approx seasons of grown stalk gained

--- src/components/Tractor/ConvertUp/ConvertUpTractorEntryForm.tsx
+++ src/components/Tractor/ConvertUp/ConvertUpTractorEntryForm.tsx
@@ -9,16 +9,20 @@ import { Separator } from "@/components/ui/Separator";
 import { SEEDS, STALK } from "@/constants/internalTokens";
 import { useTokenMap } from "@/hooks/pinto/useTokenMap";
 import useSowOrderV0Calculations from "@/hooks/tractor/useSowOrderV0Calculations";
+import { useWells } from "@/hooks/wells/wells";
 import {
   tractorTokenStrategyUtil as StrategyUtil,
   TRACTOR_CONVERT_UP_DEFAULT_CONSTRAINTS,
   TractorTokenStrategy,
+  calculateConvertUpBonusGrownStalk,
 } from "@/lib/Tractor";
 import { useFarmerSilo } from "@/state/useFarmerSilo";
+import { usePriceData } from "@/state/usePriceData";
 import { useSiloData } from "@/state/useSiloData";
 import { useMainToken } from "@/state/useTokenData";
 import { postSanitizedSanitizedValue } from "@/utils/string";
 import { getTokenIndex } from "@/utils/token";
+import { Token } from "@/utils/types";
 import { useCallback, useMemo, useState } from "react";
 import { useFormContext, useFormState, useWatch } from "react-hook-form";
 import TractorTokenStrategyDialog from "../TractorTokenStrategyDialog";
@@ -46,6 +50,7 @@ const ConvertUpTractorEntryForm = ({
   const [showTokenStrategyDialog, setShowTokenStrategyDialog] = useState(false);
   const mainToken = useMainToken();
   const tokenMap = useTokenMap();
+  const wells = useWells();
 
   const {
     errors: { minPriceToConvertUp, maxPriceToConvertUp },
@@ -147,7 +152,7 @@ const ConvertUpTractorEntryForm = ({
             <Fields.PriceRange />
             <Col className="gap-3">
               <Fields.GrownStalkPerBdvBonusBid />
-              <EstimatedSeasonsOfGrownStalk siloData={siloData} />
+              <EstimatedSeasonsOfGrownStalk siloData={siloData} wells={wells} />
             </Col>
           </>
         </Col>
@@ -225,20 +230,35 @@ const ButtonRow = ({
  * Isolated component that displays the estimated number of seasons of grown stalk that will be gained
  * given the current form values.
  */
-const EstimatedSeasonsOfGrownStalk = ({ siloData }: { siloData: ReturnType<typeof useSiloData> }) => {
+const EstimatedSeasonsOfGrownStalk = ({
+  siloData,
+  wells,
+}: {
+  siloData: ReturnType<typeof useSiloData>;
+  wells: ReturnType<typeof useWells>;
+}) => {
   const { form } = useConvertUpOrderFormContext();
+  const mainToken = useMainToken();
 
-  const value = useWatch({
-    control: form.control,
-    name: "grownStalkPerBdvBonusBid",
-  });
+  const totalConvertBdv = useWatch({ control: form.control, name: "totalBeanAmountToConvert" });
 
-  const tokenStrategies = useWatch({
-    control: form.control,
-    name: "tokenStrategy",
-  });
+  const value = useWatch({ control: form.control, name: "grownStalkPerBdvBonusBid" });
 
-  const approxGrownStalk = 0;
+  // IN TERMS OF PINTO GROWN STALK / SEASON
+  const seasonsOfGrownStalk = useMemo(() => {
+    const tokenData = siloData.tokenData.get(mainToken);
+    const seeds = tokenData?.rewards.seeds;
+    if (!seeds) {
+      return "0";
+    }
+
+    const totalConverted = postSanitizedSanitizedValue(totalConvertBdv, mainToken.decimals).tv;
+    const bonusGrownStalkPerBdv = postSanitizedSanitizedValue(value, STALK.decimals - mainToken.decimals).tv;
+    const totalGrownStalk = calculateConvertUpBonusGrownStalk(totalConverted, bonusGrownStalkPerBdv);
+    const seasons = totalGrownStalk.div(seeds);
+
+    return seasons.lt(1) ? "< 1" : seasons.toNumber().toFixed(0);
+  }, [totalConvertBdv, value, mainToken.decimals]);
 
   return (
     <Row className="w-full justify-between">
@@ -249,7 +269,7 @@ const EstimatedSeasonsOfGrownStalk = ({ siloData }: { siloData: ReturnType<typeo
           variant="outlined"
         />
       </Row>
-      <div className="pinto-sm-light text-pinto-green-4">~ {approxGrownStalk} TODO</div>
+      <div className="pinto-sm-light text-pinto-green-4">~ {seasonsOfGrownStalk}</div>
     </Row>
   );
 };
