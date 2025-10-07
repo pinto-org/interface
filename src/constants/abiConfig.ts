a42e3b510b6a0089ba2f6a3d51964aeb8663ac39
burr <burr.nim@proton.me>
2025-10-07 22:17:29 +0900

    feat: update addresss

--- src/constants/abiConfig.ts
+++ src/constants/abiConfig.ts
@@ -29,10 +29,10 @@ const ABI_CONFIG = {
     address: {
       [mainnet.id]: "0x4BEd6cb142b7d474242d87F4796387DEB9E1E1B4",
       [arbitrum.id]: "0xC218F5a782b0913931DCF502FA2aA959b36Ac9E7",
-      [base.id]: "0x13D25ABCB6a19948d35654715c729c6501230b49", // temp address
-      [localhost.id]: "0x13D25ABCB6a19948d35654715c729c6501230b49", // base
-      [TESTNET_CHAIN_ID]: "0x13D25ABCB6a19948d35654715c729c6501230b49", // base
-      [foundry.id]: "0x13D25ABCB6a19948d35654715c729c6501230b49", // base
+      [base.id]: "0xc355aac06a4739362Fe9B1640D816Ff9856Ccc35", // temp address
+      [localhost.id]: "0xc355aac06a4739362Fe9B1640D816Ff9856Ccc35", // base
+      [TESTNET_CHAIN_ID]: "0xc355aac06a4739362Fe9B1640D816Ff9856Ccc35", // base
+      [foundry.id]: "0xc355aac06a4739362Fe9B1640D816Ff9856Ccc35", // base
     },
   },
   pipeline: {
