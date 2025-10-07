960f6ca0acbde66e5a780241cd05d49fc19ba618
burr <burr.nim@proton.me>
2025-10-07 22:02:36 +0900

    feat: add convert up orders api transform

--- src/state/tractor/useTractorConvertUpOrders.ts
+++ src/state/tractor/useTractorConvertUpOrders.ts
@@ -1,16 +1,23 @@
+import { TV } from "@/classes/TokenValue";
 import { TIME_TO_BLOCKS } from "@/constants/blocks";
 import { QUERY_SETTINGS, defaultQuerySettingsMedium } from "@/constants/query";
+import { MAIN_TOKEN } from "@/constants/tokens";
 import { useProtocolAddress } from "@/hooks/pinto/useProtocolAddress";
 import {
+  ConvertUpBlueprintStruct,
   ConvertUpOrderbookEntry,
   TractorAPI,
   TractorAPIOrdersResponse,
   loadConvertUpOrderbookData,
+  transformConvertUpRequisitionEvent,
 } from "@/lib/Tractor";
+import { decodeBlueprintCallData } from "@/lib/Tractor/blueprint-decoders";
+import { convertUpBlueprintDecoder } from "@/lib/Tractor/blueprint-decoders/convert-up-decoder";
+import { getChainConstant, resolveChainId } from "@/utils/chain";
 import { HashString } from "@/utils/types.generic";
 import { isDev } from "@/utils/utils";
 import { DefaultError, QueryObserverOptions, useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useChainId, usePublicClient } from "wagmi";
 import { queryKeys } from "../queryKeys";
 
@@ -37,8 +44,8 @@ type UseTractorAPISowOrdersParams = Omit<UseTractorConvertOrderbookOptions<Tract
 const useTractorAPIConvertUpOrders = ({
   address,
   cancelled = false,
-  chainOnly,
-  enabled,
+  chainOnly = false,
+  enabled = true,
 }: UseTractorAPISowOrdersParams = {}) => {
   const chainId = useChainId();
 
@@ -48,6 +55,10 @@ const useTractorAPIConvertUpOrders = ({
     cancelled,
   } as const;
 
+  const select = useMemo(() => transformAPIOrderbookData(chainId), [chainId]);
+
+  console.log("enabled:", !!chainId && !chainOnly && !!enabled);
+
   const query = useQuery({
     queryKey: queryKeys.tractor.convertUpOrders({ ...args }),
     queryFn: async () => {
@@ -55,6 +66,7 @@ const useTractorAPIConvertUpOrders = ({
       return TractorAPI.getOrders<"CONVERT_UP_V0">({ ...args, orderType: "CONVERT_UP_V0", isConvert: true });
     },
     enabled: !!chainId && !chainOnly && !!enabled,
+    select,
     ...QUERY_SETTINGS.slow,
   });
 
@@ -66,6 +78,63 @@ const useTractorAPIConvertUpOrders = ({
 
   return query;
 };
+
+const transformAPIOrderbookData =
+  (chainId: number) => (response: TractorAPIOrdersResponse<"CONVERT_UP_V0"> | undefined) => {
+    if (!response) return { orders: [], lastUpdated: 0, totalRecords: 0 };
+
+    const mainToken = getChainConstant(resolveChainId(chainId), MAIN_TOKEN);
+
+    const res = response.orders.map((order): ConvertUpOrderbookEntry => {
+      const bd = order.blueprintData;
+      const bdvLeftToConvert = TV.fromBlockchain(bd.beansLeftToConvert, mainToken.decimals);
+
+      const availableAmount = TV.fromBlockchain(bd.cascadeAmountFunded, mainToken.decimals);
+
+      const decodedData = decodeBlueprintCallData(order.data) ?? undefined;
+      const transformed = transformConvertUpRequisitionEvent(decodedData?.params, chainId);
+
+      return {
+        requisitionType: "convertUpBlueprint",
+        decodedData: transformed ?? undefined,
+        requisition: {
+          blueprint: {
+            publisher: order.publisher,
+            data: order.data,
+            operatorPasteInstrs: order.operatorPasteInstrs,
+            maxNonce: BigInt(order.maxNonce),
+            startTime: BigInt(Math.floor(new Date(order.startTime).getTime() / 1000)),
+            endTime: BigInt(Math.floor(new Date(order.endTime).getTime() / 1000)),
+          },
+          blueprintHash: order.blueprintHash,
+          signature: order.signature,
+        },
+        withdrawalPlan: undefined,
+        blockNumber: order.publishedBlock,
+        timestamp: Number(new Date(order.publishedTimestamp).getTime()),
+        totalAvailableBdv: availableAmount,
+        amountConvertibleNextExecution: TV.ZERO, // Will be filled in later
+        currentlyConvertible: TV.ZERO,
+        // Will be filled in later
+        meetsConditions: {
+          price: false,
+          bonus: false,
+          capacity: false,
+        },
+        orderInfo: {
+          lastExecutedTimestamp: bd.lastExecutedTimestamp,
+          bdvLeftToConvert,
+        },
+        isComplete: bd.orderComplete,
+      };
+    });
+
+    return {
+      orders: res,
+      lastUpdated: response.lastUpdated,
+      totalRecords: response.totalRecords,
+    };
+  };
 
 type UseTractorConvertOrderbookOptions<T = ConvertUpOrderbookEntry[]> = {
   /** The Blueprint Publisher Address If none provided, all orders will be returned */
@@ -105,30 +174,50 @@ export function useTractorConvertUpOrderbook<T = ConvertUpOrderbookEntry[]>(
 
   const { address, chainOnly = false, enabled } = params ?? empty;
 
-  const apiOrdersQuery = useTractorAPIConvertUpOrders({
+  const { data: orders, ...ordersQuery } = useTractorAPIConvertUpOrders({
     address,
     cancelled: params?.cancelled,
     chainOnly,
     enabled,
   });
 
-  const orderQueriesError = false;
-  const lastUpdated = apiOrdersQuery.data?.lastUpdated;
+  const envExists = Boolean(import.meta.env.VITE_TRACTOR_CONVERT_URL);
+  const orderQueriesError = !!ordersQuery.error;
 
+  // check if the API data exists, is not loading, and is not an error
+  const ordersAPIDataExists = Boolean(orders?.orders && !ordersQuery.isLoading && !ordersQuery.isError);
+
+  // only run the chain query if we have a client, a max temperature, the API data exists, and we have a latest block reference.
+  const orderChainQueryEnabled = (chainOnly || ordersAPIDataExists || !envExists) && enabled;
+
+  /**
+   * If the orders API request failed, fetch since the TRACTOR_DEPLOYMENT_BLOCK
+   * otherwise,
+   * - DEV, use a 24 hour lookback to allow for forwarding seasons locally
+   * - PROD, use a 1 hour lookback
+   */
   const ordersChainQuery = useQuery({
-    queryKey: queryKeys.tractor.convertUpOrdersV0Chain(lastUpdated ?? chainOnly ? 1 : 0, params),
+    queryKey: queryKeys.tractor.convertUpOrdersV0Chain(orders?.lastUpdated ?? chainOnly ? 1 : 0, params),
     queryFn: async () => {
       if (!client) return [];
       const latestBlock = await client.getBlock({ blockTag: "latest" });
-      const lookbackBlocks = getLookbackBlocks(chainOnly, orderQueriesError, latestBlock.number, lastUpdated);
+      const lookbackBlocks = getLookbackBlocks(chainOnly, orderQueriesError, latestBlock.number, orders?.lastUpdated);
 
-      const events = await loadConvertUpOrderbookData(address, diamond, client, latestBlock, [], lookbackBlocks, {
-        filterOutCompleted: params?.filterOutCompleted ?? true,
-      });
+      const events = await loadConvertUpOrderbookData(
+        address,
+        diamond,
+        client,
+        latestBlock,
+        orders?.orders ?? [],
+        lookbackBlocks,
+        {
+          filterOutCompleted: params?.filterOutCompleted ?? true,
+        },
+      );
 
       return events;
     },
-    enabled: enabled && !!client,
+    enabled: orderChainQueryEnabled,
     select: params?.select,
     ...defaultQuerySettingsMedium,
   });
