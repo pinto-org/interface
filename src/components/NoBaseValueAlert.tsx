51b38bc16b9ac0c9aae78cce8914d96ed0a63d27
feyyazcigim <feyyazcavlak@outlook.com>
2025-12-13 03:31:23 +0300

    Add privy funding and update no value alert

--- src/components/NoBaseValueAlert.tsx
+++ src/components/NoBaseValueAlert.tsx
@@ -1,21 +1,27 @@
+import privyLogo from "@/assets/misc/Privy_Symbol_Black.png";
 import baseLogo from "@/assets/misc/base-logo.svg";
 import bridggLogo from "@/assets/misc/bridgg-logo.svg";
 import coinbaseLogo from "@/assets/misc/coinbase-logo.svg";
 import useDelayedLoading from "@/hooks/display/useDelayedLoading";
 import useFarmerStatus from "@/hooks/useFarmerStatus";
+import { useFundWallet } from "@privy-io/react-auth";
 import { Cross2Icon } from "@radix-ui/react-icons";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { base } from "viem/chains";
 import { useAccount } from "wagmi";
 import { Button } from "./ui/Button";
 import IconImage from "./ui/IconImage";
 
 export default function NoBaseValueAlert() {
   const [show, setShow] = useState(false);
-  const { isConnected } = useAccount();
+  const { isConnected, connector, address } = useAccount();
   const { loading, didLoad, hasBalanceOnBase } = useFarmerStatus();
 
   const { loading: isLoading, setLoading } = useDelayedLoading(3000, !didLoad);
+
+  const { fundWallet } = useFundWallet();
 
   useEffect(() => {
     setLoading(loading);
@@ -80,30 +86,54 @@ export default function NoBaseValueAlert() {
             </div>
             <div className="flex flex-col gap-4 p-4 sm:gap-6 sm:p-6">
               <p className="text-sm sm:text-2xl pr-2">
-                Looks like you don't have any funds on Base. Bridge value from Ethereum, or send value to your wallet
-                using an exchange.
+                {connector?.type === "privy"
+                  ? "Looks like you don't have any funds on Base. Fund your wallet via Privy, or send value to your wallet using an exchange."
+                  : "Looks like you don't have any funds on Base. Bridge value from Ethereum, or send value to your wallet using an exchange."}
               </p>
               <div className="flex flex-col lg:flex-row items-center gap-4 w-full">
-                <Button
-                  asChild
-                  className="bg-[#0052FF] hover:bg-[#0052CC] text-white rounded-2xl h-12 sm:h-20 w-full sm:flex-1 px-4 sm:px-6"
-                >
-                  <a
-                    href="https://www.brid.gg/base?amount=&originChainId=1&token=ETH"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="flex items-center gap-2 w-full"
+                {connector?.type === "privy" ? (
+                  <Button
+                    asChild
+                    variant="outline"
+                    className="bg-white text-black border-pinto-gray-2 hover:bg-gray-100 rounded-2xl h-12 sm:h-20 w-full sm:flex-1 px-4 sm:px-6 cursor-pointer"
+                    onClick={() => {
+                      if (!address) {
+                        toast.error("Wallet address not found!");
+                        return;
+                      }
+                      fundWallet({ address: address, options: { chain: base } });
+                    }}
                   >
-                    <div className="flex-shrink-0 flex items-center gap-2">
-                      <img src={baseLogo} alt="Base" className="w-6 h-6 sm:w-12 sm:h-12" />
-                      <img src={bridggLogo} alt="Brid.gg" className="w-4 h-4 sm:w-8 sm:h-8" />
+                    <div className="flex items-center gap-2 w-full">
+                      <div className="flex-shrink-0 flex items-center gap-2">
+                        <img src={privyLogo} alt="Base" className="h-6 sm:h-12" />
+                      </div>
+                      <span className="hidden sm:block text-sm sm:text-2xl text-left">Fund your wallet</span>
+                      <span className="sm:hidden text-sm sm:text-2xl text-left">Bridge to Base using Brid.gg</span>
                     </div>
-                    <span className="hidden sm:block text-sm sm:text-2xl text-left">
-                      Bridge tokens to Base using Brid.gg
-                    </span>
-                    <span className="sm:hidden text-sm sm:text-2xl text-left">Bridge to Base using Brid.gg</span>
-                  </a>
-                </Button>
+                  </Button>
+                ) : (
+                  <Button
+                    asChild
+                    className="bg-[#0052FF] hover:bg-[#0052CC] text-white rounded-2xl h-12 sm:h-20 w-full sm:flex-1 px-4 sm:px-6"
+                  >
+                    <a
+                      href="https://www.brid.gg/base?amount=&originChainId=1&token=ETH"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex items-center gap-2 w-full"
+                    >
+                      <div className="flex-shrink-0 flex items-center gap-2">
+                        <img src={baseLogo} alt="Base" className="w-6 h-6 sm:w-12 sm:h-12" />
+                        <img src={bridggLogo} alt="Brid.gg" className="w-4 h-4 sm:w-8 sm:h-8" />
+                      </div>
+                      <span className="hidden sm:block text-sm sm:text-2xl text-left">
+                        Bridge tokens to Base using Brid.gg
+                      </span>
+                      <span className="sm:hidden text-sm sm:text-2xl text-left">Bridge to Base using Brid.gg</span>
+                    </a>
+                  </Button>
+                )}
                 <Button
                   asChild
                   variant="outline"
